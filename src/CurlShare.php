4325d846c7505464be7adacf6e64a007063abd35
Greg Bowler <greg.bowler@g105b.com>
2018-07-26 11:42:22 +0100

    Implement CurlShare

---
+++ src/CurlShare.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Gt\Curl;
+
+class CurlShare implements CurlShareInterface {
+	/** @var resource */
+	protected $sh;
+
+	public function __construct() {
+		$this->init();
+	}
+
+	public function __destruct() {
+		$this->close();
+	}
+
+	/**
+	 * Return string describing the given error code
+	 * @see http://php.net/manual/en/function.curl-share-strerror.php
+	 */
+	public static function strError(int $errorNum):string {
+		return curl_share_strerror($errorNum);
+	}
+
+	/**
+	 * Close the cURL share handle
+	 * @see http://php.net/manual/en/function.curl-share-close.php
+	 */
+	public function close():void {
+		curl_share_close($this->sh);
+	}
+
+	/**
+	 * Initialize the cURL share handle
+	 * @see http://php.net/manual/en/function.curl-share-init.php
+	 */
+	public function init():void {
+		$this->sh = curl_share_init();
+	}
+
+	/**
+	 * Set an option for the cURL share handle
+	 * @see http://php.net/manual/en/function.curl-share-setopt.php
+	 */
+	public function setOpt(int $option, string $value):bool {
+		return curl_share_setopt($this->sh, $option, $value);
+	}
+}
