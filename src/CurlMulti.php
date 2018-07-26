6af2315d3b589066635e6e93e78f9d756a9f21f8
Greg Bowler <greg.bowler@g105b.com>
2018-07-26 09:32:34 +0100

    Define methods in CurlMultiInterface

---
+++ src/CurlMulti.php
@@ -0,0 +1,8 @@
+<?php
+namespace Gt\Curl;
+
+use Gt\CurlInterface\CurlMultiInterface;
+
+class CurlMulti implements CurlMultiInterface {
+
+}
--- src/CurlMultiInterface.php
+++ src/CurlMultiInterface.php
@@ -7,17 +7,15 @@ namespace Gt\CurlInterface;
  * matching methods for all internal functions.
  */
 interface CurlMultiInterface {
-	/**
-	 * Obtain the underlying Curl Multi resource, as created with curl_multi_init.
-	 */
-	public function getHandle():resource;
+	public function __construct();
+
+	public function __destruct();
 
 	/**
 	 * Returns a text error message describing the given CURLM error code.
-	 *
-	 * @see curl_multi_strerror
+	 * @see http://php.net/manual/en/function.curl-multi-strerror.php
 	 */
-	static public function strerror(int $errorNum):string;
+	public static function strerror(int $errorNum):string;
 
 	/**
 	 * @see curl_multi_add_handle()
@@ -26,33 +24,61 @@ interface CurlMultiInterface {
 	public function add(CurlInterface $curl):void;
 
 	/**
-	 * @see curl_multi_exec()
-	 * @throws CurlException if a CURLM_XXX error is made
+	 * Close the set of cURL handles
+	 * @see http://php.net/manual/en/function.curl-multi-close.php
 	 */
-	public function exec(int &$stillRunning):void;
+	public function close():void;
 
 	/**
-	 * @see curl_multi_getcontent()
+	 * Return the last multi curl error number
+	 * @return int
+	 */
+	public function errno():int;
+
+	/**
+	 * Run the sub-connections of the current cURL handle
+	 * @see http://php.net/manual/en/function.curl-multi-exec.php()
+	 * @throws CurlException if a CURLM_XXX error is made
+	 */
+	public function exec(int &$stillRunning):int;
+
+	/**
+	 * Obtain the underlying Curl Multi resource, as created with curl_multi_init.
+	 * @return resource
+	 */
+	public function getHandle();
+
+	/**
+	 * Return the content of a cURL handle if CURLOPT_RETURNTRANSFER is set
+	 * @see http://php.net/manual/en/function.curl-multi-getcontent.php
 	 * @return string the content of a cURL handle if CURLOPT_RETURNTRANSFER is set
 	 */
 	public function getContent(CurlInterface $curl):string;
 
 	/**
-	 * @see curl_multi_info_read()
+	 * Get information about the current transfers
+	 * @see http://php.net/manual/en/function.curl-multi-info-read.php
 	 * @throws CurlException on failure
 	 */
-	public function infoRead(int &$msgs = null):CurlMultiInfoInterface;
+	public function infoRead(int &$msgsInQueue = null):CurlMultiInfoInterface;
 
 	/**
-	 * @see curl_multi_remove_handle()
+	 * Initialises a new cURL multi handle
+	 * @see http://php.net/manual/en/function.curl-multi-init.php
+	 */
+	public function init():void;
+
+	/**
+	 * Remove a multi handle from the set of cURL handles
+	 * @see http://php.net/manual/en/function.curl-multi-remove-handle.php
 	 * @throws CurlException on failure
 	 */
 	public function remove(CurlInterface $curl):void;
 
 	/**
-	 * Wait for activity on any curl_multi connection.
+	 * Wait for activity on any curl_multi connection
 	 *
-	 * @see curl_multi_select()
+	 * @see http://php.net/manual/en/function.curl-multi-select.php
 	 * @throws CurlException on select failure or timeout
 	 */
 	public function select(float $timeout = 1.0):int;
@@ -60,7 +86,7 @@ interface CurlMultiInterface {
 	/**
 	 * Set an option for the cURL multi handle, one of the CURLMOPT_* constants.
 	 *
-	 * @see curl_multi_setopt
+	 * @see http://php.net/manual/en/function.curl-multi-setopt.php
 	 * @throws CurlException on failure
 	 */
 	public function setOpt(int $opt, $val):void;
