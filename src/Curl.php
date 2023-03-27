61508e556eafb769c002f0177d32bc690807973b
Greg Bowler <greg.bowler@g105b.com>
2023-03-27 16:46:01 +0100

    tweak: only close if already open

--- src/Curl.php
+++ src/Curl.php
@@ -20,7 +20,9 @@ class Curl implements CurlInterface {
 	 * @see http://php.net/manual/en/function.curl-close.php
 	 */
 	public function __destruct() {
-		curl_close($this->ch);
+		if(isset($this->ch)) {
+			curl_close($this->ch);
+		}
 	}
 
 	/**
